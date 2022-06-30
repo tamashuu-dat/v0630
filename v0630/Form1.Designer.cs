4cd095a5b5c35c964a12fa6840f40015897000a3
A601-13 <tnk.dat.class@gmail.com>
2022-06-30 15:59:00 +0900

    3つ動かす

--- v0630/Form1.Designer.cs
+++ v0630/Form1.Designer.cs
@@ -33,13 +33,15 @@ namespace v0630
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Yu Gothic UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(12, 71);
+            this.label1.Location = new System.Drawing.Point(386, 218);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(31, 25);
             this.label1.TabIndex = 0;
@@ -62,11 +64,33 @@ namespace v0630
             this.timer1.Interval = 50;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Yu Gothic UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(224, 256);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "★";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Yu Gothic UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(418, 314);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "★";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
@@ -81,6 +105,8 @@ namespace v0630
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- v0630/Form1.cs
+++ v0630/Form1.cs
@@ -12,12 +12,24 @@ namespace v0630
 {
     public partial class Form1 : Form
     {
-        int vx = -5;
-        int vy = -10;
-
+        int vx = rand.Next(-10, 11);
+        int vy = rand.Next(-10, 11);
+        int vx3 = rand.Next(-10, 11);
+        int vy3 = rand.Next(-10, 11);
+        int vx4 = rand.Next(-10, 11);
+        int vy4 = rand.Next(-10, 11);
+        // 静的=最初に決めておく <=> 動的=実行時に変更可能
+        static Random rand = new Random();
         public Form1()
         {
             InitializeComponent();
+
+            label1.Left = rand.Next(ClientSize.Width - label1.Width);
+            label1.Top = rand.Next(ClientSize.Height - label1.Height);
+            label3.Left = rand.Next(ClientSize.Width - label3.Width);
+            label3.Top = rand.Next(ClientSize.Height - label3.Height);
+            label4.Left = rand.Next(ClientSize.Width - label4.Width);
+            label4.Top = rand.Next(ClientSize.Height - label4.Height);
         }
 
         private void timer1_Tick(object sender, EventArgs e)
@@ -25,7 +37,13 @@ namespace v0630
             label1.Left += vx;
             label1.Top += vy;
 
-            if(label1.Left<0)
+            label3.Left += vx3;
+            label3.Top += vy3;
+
+            label4.Left += vx4;
+            label4.Top += vy4;
+
+            if (label1.Left<0)
             {
                 vx = Math.Abs(vx);
             }
@@ -47,11 +65,75 @@ namespace v0630
             label2.Left = fpos.X - label2.Width / 2;
             label2.Top = fpos.Y - label2.Height / 2;
 
-            if((label1.Left<=fpos.X)&&(label1.Top<=fpos.Y)&&(label1.Right>fpos.X)&&(label1.Bottom>fpos.Y))
+            if ((label1.Left <= fpos.X)
+                && (label1.Top <= fpos.Y)
+                && (label1.Right > fpos.X)
+                && (label1.Bottom > fpos.Y))
             {
                 timer1.Enabled = false;
-                //MessageBox.Show("Game Over");
             }
+
+            if (label3.Left < 0)
+            {
+                vx3 = Math.Abs(vx3);
+            }
+            if (label3.Top < 0)
+            {
+                vy3 = Math.Abs(vy3);
+            }
+            if (label3.Right > ClientSize.Width)
+            {
+                vx3 = -Math.Abs(vx3);
+            }
+            if (label3.Bottom > ClientSize.Height)
+            {
+                vy3 = -Math.Abs(vy3);
+            }
+
+            if ((label1.Left <= fpos.X)
+                && (label1.Top <= fpos.Y)
+                && (label1.Right > fpos.X)
+                && (label1.Bottom > fpos.Y))
+            {
+                timer1.Enabled = false;
+            }
+
+            if (label4.Left < 0)
+            {
+                vx4 = Math.Abs(vx4);
+            }
+            if (label4.Top < 0)
+            {
+                vy4 = Math.Abs(vy4);
+            }
+            if (label4.Right > ClientSize.Width)
+            {
+                vx4 = -Math.Abs(vx4);
+            }
+            if (label4.Bottom > ClientSize.Height)
+            {
+                vy4 = -Math.Abs(vy4);
+            }
+
+            if ((label4.Left <= fpos.X)
+                && (label4.Top <= fpos.Y)
+                && (label4.Right > fpos.X)
+                && (label4.Bottom > fpos.Y))
+            {
+                timer1.Enabled = false;
+            }
+            /*if((  label1.Left<=label2.Right)
+                &&(label1.Top<=label2.Bottom)
+                &&(label1.Right>label2.Left)
+                &&(label1.Bottom>label2.Top))
+            {
+                timer1.Enabled = false;
+                MessageBox.Show("Game Over");
+            }*/
+
+
+
+
         }
 
         private void label2_Click(object sender, EventArgs e)
