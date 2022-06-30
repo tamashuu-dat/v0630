d82e1f4815e6a767d7bab4c907eebced21443351
A601-13 <tnk.dat.class@gmail.com>
2022-06-30 13:31:51 +0900

    復習

--- v0630/Form1.Designer.cs
+++ v0630/Form1.Designer.cs
@@ -30,12 +30,54 @@ namespace v0630
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(19, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "★";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "（・ω・）";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- v0630/Form1.cs
+++ v0630/Form1.cs
@@ -12,10 +12,44 @@ namespace v0630
 {
     public partial class Form1 : Form
     {
+        int vx = -5;
+        int vy = -10;
+
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left += vx;
+            label1.Top += vy;
+            if(label1.Left<0)
+            {
+                vx = Math.Abs(vx);
+            }
+            if(label1.Top<0)
+            {
+                vy = Math.Abs(vy);
+            }
+            if(label1.Right>ClientSize.Width)
+            {
+                vx = -Math.Abs(vx);
+            }
+            if(label1.Bottom>ClientSize.Height)
+            {
+                vy = -Math.Abs(vy);
+            }
+            Point spos = MousePosition;
+            Point fpos = PointToClient(spos);
+            label2.Left = fpos.X - label2.Width / 2;
+            label2.Top = fpos.Y - label2.Height / 2;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+           
+
+        }
     }
 }
