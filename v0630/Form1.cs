3ee77160e87e276b1c1ee8f56f49ec557f8b3161
A601-13 <tnk.dat.class@gmail.com>
2022-06-30 16:07:15 +0900

    配列化

--- v0630/Form1.cs
+++ v0630/Form1.cs
@@ -12,17 +12,21 @@ namespace v0630
 {
     public partial class Form1 : Form
     {
-        int vx = rand.Next(-10, 11);
-        int vy = rand.Next(-10, 11);
-        int vx3 = rand.Next(-10, 11);
-        int vy3 = rand.Next(-10, 11);
-        int vx4 = rand.Next(-10, 11);
-        int vy4 = rand.Next(-10, 11);
+        int [] vx = new int[3];
+        int [] vy = new int[3];
+        
         // 静的=最初に決めておく <=> 動的=実行時に変更可能
         static Random rand = new Random();
         public Form1()
         {
             InitializeComponent();
+
+            vx[0] = rand.Next(-10, 11);
+            vy[0] = rand.Next(-10, 11);
+            vx[1] = rand.Next(-10, 11);
+            vy[1] = rand.Next(-10, 11);
+            vx[2] = rand.Next(-10, 11);
+            vy[2] = rand.Next(-10, 11);
 
             label1.Left = rand.Next(ClientSize.Width - label1.Width);
             label1.Top = rand.Next(ClientSize.Height - label1.Height);
@@ -34,30 +38,30 @@ namespace v0630
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            label1.Left += vx;
-            label1.Top += vy;
+            label1.Left += vx[0];
+            label1.Top += vy[0];
 
-            label3.Left += vx3;
-            label3.Top += vy3;
+            label3.Left += vx[1];
+            label3.Top += vy[1];
 
-            label4.Left += vx4;
-            label4.Top += vy4;
+            label4.Left += vx[2];
+            label4.Top += vy[2];
 
             if (label1.Left<0)
             {
-                vx = Math.Abs(vx);
+                vx[0] = Math.Abs(vx[0]);
             }
             if(label1.Top<0)
             {
-                vy = Math.Abs(vy);
+                vy[0] = Math.Abs(vy[0]);
             }
             if(label1.Right>ClientSize.Width)
             {
-                vx = -Math.Abs(vx);
+                vx[0] = -Math.Abs(vx[0]);
             }
             if(label1.Bottom>ClientSize.Height)
             {
-                vy = -Math.Abs(vy);
+                vy[0] = -Math.Abs(vy[0]);
             }
 
             Point spos = MousePosition;
@@ -75,44 +79,44 @@ namespace v0630
 
             if (label3.Left < 0)
             {
-                vx3 = Math.Abs(vx3);
+                vx[1] = Math.Abs(vx[1]);
             }
             if (label3.Top < 0)
             {
-                vy3 = Math.Abs(vy3);
+                vy[1] = Math.Abs(vy[1]);
             }
             if (label3.Right > ClientSize.Width)
             {
-                vx3 = -Math.Abs(vx3);
+                vx[1] = -Math.Abs(vx[1]);
             }
             if (label3.Bottom > ClientSize.Height)
             {
-                vy3 = -Math.Abs(vy3);
+                vy[1] = -Math.Abs(vy[1]);
             }
 
-            if ((label1.Left <= fpos.X)
-                && (label1.Top <= fpos.Y)
-                && (label1.Right > fpos.X)
-                && (label1.Bottom > fpos.Y))
+            if ((label3.Left <= fpos.X)
+                && (label3.Top <= fpos.Y)
+                && (label3.Right > fpos.X)
+                && (label3.Bottom > fpos.Y))
             {
                 timer1.Enabled = false;
             }
 
             if (label4.Left < 0)
             {
-                vx4 = Math.Abs(vx4);
+                vx[2] = Math.Abs(vx[2]);
             }
             if (label4.Top < 0)
             {
-                vy4 = Math.Abs(vy4);
+                vy[2] = Math.Abs(vy[2]);
             }
             if (label4.Right > ClientSize.Width)
             {
-                vx4 = -Math.Abs(vx4);
+                vx[2] = -Math.Abs(vx[2]);
             }
             if (label4.Bottom > ClientSize.Height)
             {
-                vy4 = -Math.Abs(vy4);
+                vy[2] = -Math.Abs(vy[2]);
             }
 
             if ((label4.Left <= fpos.X)
@@ -122,6 +126,7 @@ namespace v0630
             {
                 timer1.Enabled = false;
             }
+
             /*if((  label1.Left<=label2.Right)
                 &&(label1.Top<=label2.Bottom)
                 &&(label1.Right>label2.Left)
@@ -130,10 +135,7 @@ namespace v0630
                 timer1.Enabled = false;
                 MessageBox.Show("Game Over");
             }*/
-
-
-
-
+            
         }
 
         private void label2_Click(object sender, EventArgs e)
