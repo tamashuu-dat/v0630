23f7a7a4a172c66f066d3fc0fea7cd0f3227aea8
A601-13 <tnk.dat.class@gmail.com>
2022-07-07 15:13:11 +0900

    100個移動

--- v0630/Form1.cs
+++ v0630/Form1.cs
@@ -12,42 +12,82 @@ namespace v0630
 {
     public partial class Form1 : Form
     {
-        int [] vx = new int[3];
-        int [] vy = new int[3];
-        
+        int [] vx = new int[100];
+        int [] vy = new int[100];
+        Label[] labels = new Label[100];//スペースの確保
+
         // 静的=最初に決めておく <=> 動的=実行時に変更可能
         static Random rand = new Random();
         public Form1()
         {
             InitializeComponent();
 
-            vx[0] = rand.Next(-10, 11);
-            vy[0] = rand.Next(-10, 11);
-            vx[1] = rand.Next(-10, 11);
-            vy[1] = rand.Next(-10, 11);
-            vx[2] = rand.Next(-10, 11);
-            vy[2] = rand.Next(-10, 11);
+            /*for(int ii=0;ii<3;ii++)
+            {
+                MessageBox.Show($"{ii}");//for文の確認
+            }*/
 
-            label1.Left = rand.Next(ClientSize.Width - label1.Width);
+            for (int i = 0; i < 100; i++)
+            {
+                vx[i] = rand.Next(-10, 11);
+                vy[i] = rand.Next(-10, 11);
+                labels[i] = new Label();//実体の準備
+                labels[i].AutoSize = true;
+                labels[i].Text = "★";
+                Controls.Add(labels[i]);
+
+                labels[i].Left = rand.Next(ClientSize.Width - labels[i].Width);
+                labels[i].Top = rand.Next(ClientSize.Height - labels[i].Height);
+            }
+
+            /*label1.Left = rand.Next(ClientSize.Width - label1.Width);
             label1.Top = rand.Next(ClientSize.Height - label1.Height);
             label3.Left = rand.Next(ClientSize.Width - label3.Width);
             label3.Top = rand.Next(ClientSize.Height - label3.Height);
             label4.Left = rand.Next(ClientSize.Width - label4.Width);
-            label4.Top = rand.Next(ClientSize.Height - label4.Height);
+            label4.Top = rand.Next(ClientSize.Height - label4.Height);*/
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            label1.Left += vx[0];
-            label1.Top += vy[0];
+            for(int i=0;i<100;i++)
+            {
+                labels[i].Left += vx[i];
+                labels[i].Top += vy[i];
+                if(labels[i].Left<0)
+                {
+                    vx[i] = Math.Abs(vx[i]);
+                }
+                if(labels[i].Top<0)
+                {
+                    vy[i] = Math.Abs(vy[i]);
+                }
+                if (labels[i].Right > ClientSize.Width)
+                {
+                    vx[i] = -Math.Abs(vx[i]);
+                }
+                if(labels[i].Bottom>ClientSize.Height)
+                {
+                    vy[i] = -Math.Abs(vy[i]);
+                }
+            }
+            Point spos = MousePosition;
+            Point fpos = PointToClient(spos);
+            label2.Left = fpos.X - label2.Width / 2;
+            label2.Top = fpos.Y - label2.Height / 2;
 
-            label3.Left += vx[1];
-            label3.Top += vy[1];
+            for(int i=0;i<100;i++)
+            {
+                if ((labels[i].Left <= fpos.X)
+               && (labels[i].Top <= fpos.Y)
+               && (labels[i].Right > fpos.X)
+               && (labels[i].Bottom > fpos.Y))
+                {
+                    labels[i].Visible = false;
+                }
+            }
 
-            label4.Left += vx[2];
-            label4.Top += vy[2];
-
-            if (label1.Left<0)
+            /*if (label1.Left<0)
             {
                 vx[0] = Math.Abs(vx[0]);
             }
@@ -62,14 +102,11 @@ namespace v0630
             if(label1.Bottom>ClientSize.Height)
             {
                 vy[0] = -Math.Abs(vy[0]);
-            }
+            }*/
 
-            Point spos = MousePosition;
-            Point fpos = PointToClient(spos);
-            label2.Left = fpos.X - label2.Width / 2;
-            label2.Top = fpos.Y - label2.Height / 2;
 
-            if ((label1.Left <= fpos.X)
+
+            /*if ((label1.Left <= fpos.X)
                 && (label1.Top <= fpos.Y)
                 && (label1.Right > fpos.X)
                 && (label1.Bottom > fpos.Y))
@@ -125,7 +162,7 @@ namespace v0630
                 && (label4.Bottom > fpos.Y))
             {
                 timer1.Enabled = false;
-            }
+            }*/
 
             /*if((  label1.Left<=label2.Right)
                 &&(label1.Top<=label2.Bottom)
